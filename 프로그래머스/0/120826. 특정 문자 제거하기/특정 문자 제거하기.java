275d49591b01d5a0325b40f337caf662933aec5e
jjoonyna <162095326+jjoonyna@users.noreply.github.com>
2024-10-09 13:37:00 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 41.61 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        String answer = "";
+        for(int i=0; i<my_string.length(); i++) {
+            if(letter.charAt(0)==my_string.charAt(i)){
+            	continue;
+            }else {
+            	answer += my_string.charAt(i);				            	
+            }
+		}
+        return answer;
+    }
+}
