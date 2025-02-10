44ff8aebe9f883661d103da47ec02bcc6cab0f64
jjoonyna <162095326+jjoonyna@users.noreply.github.com>
2025-02-10 21:19:10 +0900

    [level 0] Title: 리스트 자르기, Time: 0.04 ms, Memory: 86.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        int a = slicer[0];
+        int b = slicer[1];
+        int c= 1;
+        switch(n){
+            case 1 : a = 0; break;
+            case 2 : b = num_list.length-1; break;
+            case 4 : c = slicer[2]; break;
+        }
+        int size = (b-a)/c+1;
+        int[] answer = new int[size];
+        int index = 0;
+        for(int i=a; i<b+1; i+=c){
+            answer[index++] = num_list[i];
+        }
+        return answer;
+    }
+}
