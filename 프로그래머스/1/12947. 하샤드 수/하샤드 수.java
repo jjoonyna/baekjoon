a0a6eb5eeac96615eafc5308ed9af9274adcc120
jjoonyna <162095326+jjoonyna@users.noreply.github.com>
2025-02-27 21:24:49 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = false;
+        int num = x;
+        int sum = 0;
+        
+        while(num>0){
+            sum += num%10;
+            num = num/10;
+        }
+        
+        if(x%sum==0){
+            answer = true;
+        }
+        
+        return answer;
+    }
+}
