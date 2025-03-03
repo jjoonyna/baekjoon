57d38b4991626ad486a6ec1ac2e54e0c53417550
jjoonyna <162095326+jjoonyna@users.noreply.github.com>
2025-03-04 01:01:20 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.13 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean solution(String s) {
+        boolean answer = false;
+        if(s.length()==4 || s.length()==6){
+            s = s.replaceAll("[0-9]","");
+            answer = s.length()>0 ? false : true;
+        }
+        return answer;
+    }
+}
