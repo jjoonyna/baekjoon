d6c29acece83f6977c586ad88a322f57d2e35b21
jjoonyna <162095326+jjoonyna@users.noreply.github.com>
2025-04-07 09:18:42 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 192 ms, Memory: 17928 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+        	String str = sc.nextLine();
+        	System.out.println(str);
+        }
+		
+    }
+}
